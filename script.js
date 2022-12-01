50a60f269c78a59d1460558d379722faef7e1d18
Corentin Miserque <miserquecorentin@gmail.com>
2022-12-01 14:33:17 +0100

    modif

---
+++ script.js
@@ -0,0 +1,45 @@
+const collection = [
+    {
+        name: "Elite",
+        director:"	Darío Madrona",
+        years:"2018",
+        picture: "https://static.wikia.nocookie.net/elite/images/b/b2/Ff76WxLUYAAFH0V.jpg/revision/latest/scale-to-width-down/700?cb=20221028132857&path-prefix=fr",
+        genre: "Suspense",
+        summary:"Las Encinas est une école privée huppée d’Espagne réservée aux enfants de l’élite. Mais quand leur lycée est détruit, trois adolescents issus de la classe populaire (Samuel, Nadia et Christian) y sont intégrés. Cependant, ils vont vite regretter cette opportunité... La confrontation entre les privilégiés et la classe ouvrière conduira à un meurtre. Qui est le véritable coupable ? Un des nouveaux venus ou quelqu'un d'autre ?",
+        logo:"https://www.kritiky.cz/wp-content/uploads/2019/03/OPEN_WORLD_INTL_PAYOFF_CZECH_REPUBLIC.jpg",
+        link:"https://www.netflix.com/watch/81572773?trackId=14170035",
+    },
+    {
+        name: "Warior Run",
+        director:"Simon Barry",
+        years:"2020",
+        picture: "https://fr.web.img3.acsta.net/pictures/20/06/18/09/35/0755176.jpg",
+        genre: "Action",
+        summary:"Une jeune femme d'une vingtaine d'années se réveille à la morgue, vivante, avec un artefact implanté dans le dos. Elle découvre qu'elle est maintenant membre d'un ordre ancien qui s'est donné pour mission de combattre les démons sur Terre.",
+        logo:"https://www.kritiky.cz/wp-content/uploads/2019/03/OPEN_WORLD_INTL_PAYOFF_CZECH_REPUBLIC.jpg",
+        link:"https://www.netflix.com/watch/81017338?trackId=14170035",
+    },
+    {
+        name: "Blood and water",
+        director:"	Gambit Films",
+        years:"2020",
+        picture: "https://www.bellanaija.com/wp-content/uploads/2020/05/Blood-and-Water-2-750x600.jpeg",
+        genre: "Suspense",
+        summary:"Une adolescente obtient son transfert dans le même lycée qu'une jeune fille qu'elle soupçonne d'être sa soeur, enlevée à la naissance 17 ans auparavant.",
+        logo:"https://www.kritiky.cz/wp-content/uploads/2019/03/OPEN_WORLD_INTL_PAYOFF_CZECH_REPUBLIC.jpg",
+        link:"https://www.netflix.com/watch/81256379?trackId=14292320&tctx=1%2C5%2Ca82f484b-a1c3-4c3d-a320-7522000b414e-48877482%2CNES_86FD435FD02AD6E928D430D8EF7363-1D364BE54DB1AF-BB3149E049_p_1669898948224%2CNES_86FD435FD02AD6E928D430D8EF7363_p_1669898948224%2C%2C%2C%2C",
+    },
+    {
+        name: "Blood and water",
+        director:"	Gambit Films",
+        years:"2020",
+        picture: "https://www.bellanaija.com/wp-content/uploads/2020/05/Blood-and-Water-2-750x600.jpeg",
+        genre: "Suspense",
+        summary:"Une adolescente obtient son transfert dans le même lycée qu'une jeune fille qu'elle soupçonne d'être sa soeur, enlevée à la naissance 17 ans auparavant.",
+        logo:"https://www.kritiky.cz/wp-content/uploads/2019/03/OPEN_WORLD_INTL_PAYOFF_CZECH_REPUBLIC.jpg",
+        link:"https://www.netflix.com/watch/81256379?trackId=14292320&tctx=1%2C5%2Ca82f484b-a1c3-4c3d-a320-7522000b414e-48877482%2CNES_86FD435FD02AD6E928D430D8EF7363-1D364BE54DB1AF-BB3149E049_p_1669898948224%2CNES_86FD435FD02AD6E928D430D8EF7363_p_1669898948224%2C%2C%2C%2C",
+    },
+]
+
+let newDiv = document.createElement("div");
+newDiv.textContent = "bonjour" +1
